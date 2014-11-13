c019e726bdd896ba2c60766e499fa8b746710bc2
jalvz <ihavetheblues@gmail.com>
2014-11-13 18:51:27 +0100

    Refactor project layout

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="conssert",
-    version="0.1",
+    version="0.2",
     description="Content assertion library for Python",
     url="https://github.com/podio/conssert",
-    download_url="https://github.com/podio/conssert/tarball/0.1",
+    download_url="https://github.com/podio/conssert/tarball/0.2",
     author="Juan Alvarez",
     author_email="juan.afernandez@ymail.com",
     platforms=["any"],
+    packages=find_packages(exclude="tests"),
     keywords=["validation", "test", "unit test", "content assertion"],
     classifiers=[
         "Development Status :: 4 - Beta",
