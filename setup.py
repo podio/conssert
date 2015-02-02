bd68e4897feb2b94004318dc0f3b9524e24cf33f
jalvz <ihavetheblues@gmail.com>
2015-02-02 23:07:38 +0100

    update version

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="conssert",
-    version="0.2.9",
+    version="0.2.10",
     description="Content assertion library for Python",
     url="https://github.com/podio/conssert",
-    download_url="https://github.com/podio/conssert/tarball/0.2.9",
+    download_url="https://github.com/podio/conssert/tarball/0.2.10",
     author="Juan Alvarez",
     author_email="juan.afernandez@ymail.com",
     platforms=["any"],
